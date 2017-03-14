cc65663c050050308e127544e65d54eacc58a6df
Netflux <netfluxer@gmail.com>
2017-03-14 11:10:06 +0800

    Updated main content to push to the side when docked side drawer is opened

--- src/common/ui/pages/BasePage.jsx
+++ src/common/ui/pages/BasePage.jsx
@@ -1,26 +1,45 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
 import Helmet from 'react-helmet'
 
 import SideDrawer from '../components/SideDrawer'
 
-export default class BasePage extends React.Component {
-	render() {
-		return (
-			<div id="content">
-				<Helmet
-					htmlAttributes={{"lang": "en"}}
-					link={[
-						{rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css"},
-						{rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css"},
-						{rel: "stylesheet", href: "https://fonts.googleapis.com/icon?family=Roboto|Material+Icons"},
-						{rel: "stylesheet", href: "/static/style.css"}
-					]}
-				/>
-
-				{this.props.children}
-
-				<SideDrawer />
-			</div>
-		)
+const mapStateToProps = state => {
+	return {
+		isOpen: state.sideDrawerState.isOpen,
+		isDocked: state.sideDrawerState.isDocked
 	}
 }
+
+const mapDispatchToProps = dispatch => ({})
+
+// Define the Base Page component
+const BasePageComponent = ({children, isOpen, isDocked}) => (
+	<div id="content" className={isOpen && isDocked ? "content-offset" : ""}>
+		<Helmet
+			htmlAttributes={{"lang": "en"}}
+			link={[
+				{rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css"},
+				{rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css"},
+				{rel: "stylesheet", href: "https://fonts.googleapis.com/icon?family=Roboto|Material+Icons"},
+				{rel: "stylesheet", href: "/static/style.css"}
+			]}
+		/>
+
+		{children}
+
+		<SideDrawer />
+	</div>
+)
+
+// Define the property types that the component expects to receive
+BasePageComponent.propTypes = {
+	children: PropTypes.object.isRequired,
+	isOpen: PropTypes.bool.isRequired,
+	isDocked: PropTypes.bool.isRequired
+}
+
+// Define the container for the Base Page component (maps state and dispatchers)
+const BasePage = connect(mapStateToProps, mapDispatchToProps)(BasePageComponent)
+
+export default BasePage
