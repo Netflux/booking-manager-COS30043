d1ba4cd02918a976778b4e8276fcc0fced553462
Netflux <netfluxer@gmail.com>
2017-03-14 11:46:24 +0800

    Updated side drawer to automatically open and dock when window width is 1024px or greater

--- src/common/ui/components/SideDrawer.jsx
+++ src/common/ui/components/SideDrawer.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 import { Drawer, MenuItem, Toolbar, ToolbarTitle } from 'material-ui'
 import { white } from 'material-ui/styles/colors'
 
-import { toggleDrawerOpen } from '../../actions'
+import { toggleDrawerOpen, toggleDrawerDocked } from '../../actions'
 
 const mapStateToProps = state => {
 	return {
@@ -19,28 +19,68 @@ const mapDispatchToProps = dispatch => {
 			if (shouldToggle) {
 				dispatch(toggleDrawerOpen())
 			}
+		},
+		onWindowResize: (isOpen, isDocked) => {
+			if (window.innerWidth >= 1024) {
+				if (!isOpen) dispatch(toggleDrawerOpen())
+				if (!isDocked) dispatch(toggleDrawerDocked())
+			} else {
+				if (isOpen) dispatch(toggleDrawerOpen())
+				if (isDocked) dispatch(toggleDrawerDocked())
+			}
 		}
 	}
 }
 
-// Define the Side Drawer component
-const SideDrawerComponent = ({isOpen, isDocked, onToggleMenu}) => (
-	<Drawer className="side-drawer" open={isOpen} docked={isDocked} onRequestChange={() => onToggleMenu(true)}>
-		<Toolbar className="menu-bar">
-			<ToolbarTitle text="Navigation" style={{color: white}} />
-		</Toolbar>
+class SideDrawerComponent extends React.Component {
+	constructor(props) {
+		super(props)
 
-		<Link to="/" onTouchTap={() => onToggleMenu(!isDocked)}><MenuItem>Home</MenuItem></Link>
-		<Link to="/about" onTouchTap={() => onToggleMenu(!isDocked)}><MenuItem>About</MenuItem></Link>
-		<Link to="/login" onTouchTap={() => onToggleMenu(!isDocked)}><MenuItem>Login</MenuItem></Link>
-	</Drawer>
-)
+		// Bind "this" to the function
+		this.handleResize = this.handleResize.bind(this)
+	}
+
+	handleResize(event) {
+		// Set a timeout of 250ms for handling window resize events
+		if (!this.resizeTimer) {
+			this.props.onWindowResize(this.props.isOpen, this.props.isDocked)
+
+			this.resizeTimer = setTimeout(() => {
+				this.resizeTimer = false
+			}, 250)
+		}
+	}
+
+	componentDidMount() {
+		window.addEventListener('resize', this.handleResize)
+		this.handleResize()
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.handleResize)
+	}
+
+	render() {
+		return (
+			<Drawer className="side-drawer" open={this.props.isOpen} docked={this.props.isDocked} onRequestChange={() => this.props.onToggleMenu(true)}>
+				<Toolbar className="menu-bar">
+					<ToolbarTitle text="Navigation" style={{color: white}} />
+				</Toolbar>
+
+				<Link to="/" onTouchTap={() => this.props.onToggleMenu(!this.props.isDocked)}><MenuItem>Home</MenuItem></Link>
+				<Link to="/about" onTouchTap={() => this.props.onToggleMenu(!this.props.isDocked)}><MenuItem>About</MenuItem></Link>
+				<Link to="/login" onTouchTap={() => this.props.onToggleMenu(!this.props.isDocked)}><MenuItem>Login</MenuItem></Link>
+			</Drawer>
+		)
+	}
+}
 
 // Define the property types that the component expects to receive
 SideDrawerComponent.propTypes = {
 	isOpen: PropTypes.bool.isRequired,
 	isDocked: PropTypes.bool.isRequired,
-	onToggleMenu: PropTypes.func.isRequired
+	onToggleMenu: PropTypes.func.isRequired,
+	onWindowResize: PropTypes.func.isRequired
 }
 
 // Define the container for the Side Drawer component (maps state and dispatchers)
